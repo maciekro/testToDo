b0bfa278f63aa993808b5085f41ece1a7bf5030a
Maciek R <maciej.rogala@dgsoftware.pl>
2024-05-30 20:32:57 +0200

    more tests

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ test scenarios for https://todomvc.com/examples/react/dist/
 
 Precon:
 
-1. app is running and available in browswer
-2. main page alredy contains tasks
+1. app is running and available in browser
+2. main page already contains tasks
 
 3. Add new task to the list
 
@@ -12,26 +12,19 @@ Precon:
 -   click enter
 -   check if new task is available on the list
 
-2. Edit already existing task
-
--   double click on already existing task
--   change name
--   click eneter
--   check if new name is visible
-
-3. Mark task as completed
+4. Mark task as completed
 
 -   find certain task on the list
 -   click checkbox located on the left next to the chosen task
 -   check if task marked as completed is crossed out
 
-4. Delete existing task
+5. Delete existing task
 
 -   find certain task on the list
 -   click deleting cross on the right side of the chosen task
 -   check if task is deleted from the list
 
-5. Delete all tasks from the list
+6. Delete all tasks from the list
 
 -   click dropdown button on the left of text area
 -   check if all task have been removed from the list
--- cypress/e2e/toDo.cy.js
+++ cypress/e2e/toDo.cy.js
@@ -1,15 +1,28 @@
 describe('toDo app', () => {
+	beforeEach(() => {
+		cy.visit('https://todomvc.com/examples/react/dist/')
+	})
 	it('open page', () => {
 		cy.visit('https://todomvc.com/examples/react/dist/')
 	})
 
 	it('add new task and verify', () => {
 		const randomText = Math.random().toString(36).substring(2, 12)
-		cy.visit('https://todomvc.com/examples/react/dist/')
 		cy.get('[data-testid="text-input"]').type(`${randomText}{enter}`)
 		cy.get('[data-testid="todo-item-label"]').invoke('text').should('eq', randomText)
+		cy.wait(300)
 	})
 	it('editing already existing task', () => {
-		cy.visit('https://todomvc.com/examples/react/dist/')
+		const randomText = Math.random().toString(36).substring(2, 12)
+		cy.get('[data-testid="text-input"]').type(`${randomText}{enter}`)
+		cy.get('[data-testid="text-input"]').type(`${randomText}{enter}`)
+	})
+	it.only('mark as completed', () => {
+		const randomText = Math.random().toString(36).substring(2, 12)
+		cy.get('[data-testid="text-input"]').type(`${randomText}{enter}`)
+		cy.get('[data-testid="text-input"]').type(`${randomText}{enter}`)
+		cy.get(':nth-child(2) > .view > [data-testid="todo-item-toggle"]').click()
+		cy.get('[data-testid="footer-navigation"] > :nth-child(3) > a').click()
+		cy.get('[data-testid="todo-item-label"]').should('have.css', 'text-decoration').and('include', 'line-through')
 	})
 })
